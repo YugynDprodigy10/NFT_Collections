633f5d01a7c4d98767537f01d48ae50160ac3872
Dprodigy <eutaaba@gmail.com>
2022-09-15 15:12:20 +0000

    Fixed issues

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd93e3880dFdF1e341a2d04F97e753aA7b9DBBbD1";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
