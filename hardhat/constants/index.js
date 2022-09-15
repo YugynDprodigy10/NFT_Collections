ffc2c6cb7e669bc8590ce78be253a0975405a47a
Dprodigy <eutaaba@gmail.com>
2022-09-15 15:18:39 +0000

    Addes opensea api endpoint

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xd93e3880dFdF1e341a2d04F97e753aA7b9DBBbD1";
+const WHITELIST_CONTRACT_ADDRESS = "0x04fA0ca4950Fa6ECC10EFa4144dB00BB061A7Ab7";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collections-three.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
